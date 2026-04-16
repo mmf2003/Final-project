f1b835d9af17628215727371c8f2dc0f0e06b33d
Sidelnikov Oleksandr <mmf2003@ukr.netr>
2026-04-16 23:19:09 +0300

    Some changes in JS file, i Love JS - no errors

--- script.js
+++ script.js
@@ -1,8 +1,8 @@
   const circles = document.querySelectorAll('.circle');
 
-  circles.forEach(dot => {
-    dot.addEventListener('click', () => {
+  circles.forEach(circle => {
+    circle.addEventListener('click', () => {
       circles.forEach(item => item.classList.remove('active'));
-      dot.classList.add('active');
+      circle.classList.add('active');
     });
   });
